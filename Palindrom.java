bc457992bf4501bc67257dc4547fd6d2642a29b2
janjanurjaman <85498714+janjanurjaman@users.noreply.github.com>
2022-05-28 21:37:46 +0700

    program palinfrom
    ini merupakan program palindrom menggunakan bahasa pemrograman java

---
+++ Palindrom.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.janjan.kawahedukasi;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author janjan
+ */
+public class Palindrom {
+    public static void main(String[] args) {
+        String kata1, kata2;
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Kata 1 = ");
+        kata1 = input.nextLine();
+        
+        System.out.print("Kata 2 = ");
+        kata2 = input.nextLine();
+        
+        String temp = "", temp1 = "";
+        for (int i = kata1.length()-1; i >= 0; i--) {
+            temp += kata1.charAt(i);
+        }
+        System.out.print("\nPalindrom kata 1 = "+temp.toLowerCase());
+        
+        for (int i = kata2.length()-1; i >= 0; i--) {
+            temp1 += kata2.charAt(i);
+        }
+        System.out.print("\nPalindrom kata 2 = "+temp1.toLowerCase());
+        
+    }
+}
